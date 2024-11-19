e62aaa9d04f5dc37ccb489a4a5efc65d8b5f8309
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-19 09:37:27 +0000

    Updated AesEncryption
    1. Imported java.util.scanner
    2. Added menu (no functionality)
    3. Implemented methods depending on menu options (no functionality)

--- src/AesApplication.java
+++ src/AesApplication.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class AesApplication {
 
+    static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // testing...
+        String[] menuOptions = new String[3];
+        menuOptions[0] = "Encrypt file";
+        menuOptions[1] = "Decrypt file";
+        menuOptions[2] = "Quit application";
+
+        UtilityClass.menu(menuOptions);
+    }
+
+    public static void encryptFile() {
+        // encrypting code here...
+    }
+
+    public static void decryptFile() {
+        // decrypting code here...
     }
 }
