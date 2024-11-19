72078212fdf23c71e531f54deedda30382902036
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-19 09:51:53 +0000

    Updated functionality
    1. Moved menu to its own method
    2. Added user input for meu options
    3. Moved menuOptions[] into menu method
    4. Code will now keep going until user decides to quit the application (int choice == 3)

--- src/AesApplication.java
+++ src/AesApplication.java
@@ -3,21 +3,41 @@ import java.util.Scanner;
 public class AesApplication {
 
     static Scanner scanner = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
+        System.out.println("AES Encryption Simulator");
+        menu();
+    }
+
+    public static void menu() {
         String[] menuOptions = new String[3];
         menuOptions[0] = "Encrypt file";
         menuOptions[1] = "Decrypt file";
         menuOptions[2] = "Quit application";
 
+        System.out.println("Please choose one of the following (1-3)");
         UtilityClass.menu(menuOptions);
+
+        int choice = scanner.nextInt();
+
+        if(choice == 1) {
+            encryptFile();
+        }
+        else if(choice == 2) {
+            decryptFile();
+        }
+        else {
+            System.out.println("Quitting application...\nSession ended, goodbye!");
+        }
     }
 
     public static void encryptFile() {
-        // encrypting code here...
+        System.out.println("Encrypting code here\n");
+        menu();
     }
 
     public static void decryptFile() {
-        // decrypting code here...
+        System.out.println("Decrypting code here\n");
+        menu();
     }
 }
